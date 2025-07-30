34984c62bdf40469c4c3809eae0bcf8568ea7bc3
adityapraja <206730797+adityapraja@users.noreply.github.com>
2025-07-30 03:26:27 +0000

    struct intialization and createNode Func.

--- adsaexp/bst.c
+++ adsaexp/bst.c
@@ -3,10 +3,35 @@
 
 struct node {
     int data;
-    struct node *left;
-    struct node *right;
+    struct node* left;
+    struct node* right;
 } *root = NULL;
 
 void main(){
 
+}
+
+/*void insert(int n){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    struct node* parent;
+    struct node* current;
+    temp->data=n;
+    temp->left=NULL;
+    temp->right=NULL;
+    
+    if(root=NULL){
+        root=temp;
+    }
+
+    else{
+
+    }
+
+}*/
+
+struct node* createNode(int value){
+    struct node* newNode=(struct node*)malloc(sizeof(struct node));
+    newNode->data=value;
+    newNode->left=newNode->right=NULL;
+    return newNode;
 }
