3784bb67bdb319f8377565aa7346b36becac9fbd
adityapraja <206730797+adityapraja@users.noreply.github.com>
2025-08-12 17:44:34 +0000

    Completed all codes.

--- adsaexp/bst.c
+++ adsaexp/bst.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node {
+struct node             //creation of struc node
+{
     int data;
     struct node* left;
     struct node* right;
 }*root = NULL;
-
+//functions definations
 struct node* insert(struct node* root, int value);
 struct node* createNode(int value);
 void preorder(struct node* root);
@@ -14,91 +15,85 @@ void inorder(struct node* root);
 void postorder(struct node* root);
 
 
-void main(){
-
+void main()
+{
     int n;
     printf("Enter the number of elements to be inserted :");
     scanf("%d",&n);
     int a[n];
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
+    {
         scanf("%d",&a[i]);
     }
-    for(int i=0;i<n;i++){
-        root=insert(root,a[i]);
+    for(int i=0;i<n;i++)
+    {
+        root=insert(root,a[i]);         //insertion of elements in bst
     }
-    printf("Preorder Traversal:");
+    printf("Preorder Traversal:");      //traversals
     preorder(root);
     printf("\n");
+
     printf("Postorder Traversal:");
     postorder(root);
     printf("\n");
+
     printf("Inorder Traversal:");
     inorder(root);
+    printf("\n");
 }
 
-/*void insert(int n){
-    struct node* temp = (struct node*)malloc(sizeof(struct node));
-    struct node* parent;
-    struct node* current;
-    temp->data=n;
-    temp->left=NULL;
-    temp->right=NULL;
-    
-    if(root=NULL){
-        root=temp;
-    }
-
-    else{
-
-    }
-
-}*/
-
-struct node* createNode(int value){
+struct node* createNode(int value)          //creates the node if node doesnt exists
+{
     struct node* newNode=(struct node*)malloc(sizeof(struct node));
     newNode->data=value;
     newNode->left=newNode->right=NULL;
     return newNode;
 }
 
-struct node* insert(struct node* root, int value){
-    if(root == NULL){
+struct node* insert(struct node* root, int value)
+{
+    if(root == NULL)                //inserts based on condition of binary properties
+    {
         return createNode(value);
     }
-    if(value<root->data){
+    if(value<root->data)
+    {
         root->left=insert(root->left,value);
     }
-    else{
+    else
+    {
         root->right=insert(root->right, value);
     }
     return root;
 
 }
 
-void preorder(struct node* root){
+void preorder(struct node* root)        //preorder traversal Root-L-R
+{   
     if(root != NULL)
- {
- printf("%d\t", root->data);
- preorder(root->left);
- preorder(root->right);
- }
+    {
+    printf("\t%d", root->data);
+    preorder(root->left);
+    preorder(root->right);
+    }
 }
 
-void inorder(struct node* root){
+void inorder(struct node* root)     //inorder traversal L-Root-R
+{
     if(root != NULL)
- {
-
- inorder(root->left);
-  printf("%d\t", root->data);
- inorder(root->right);
- }
+    {
+    inorder(root->left);
+    printf("\t%d", root->data);
+    inorder(root->right);
+    }
 }
 
-void postorder(struct node* root){
+void postorder(struct node* root)       //postorder traversal L-R-Root
+{
     if(root != NULL)
- {
+    {
     postorder(root->left);
     postorder(root->right);
-    printf("%d\t", root->data);
- }
+    printf("\t%d", root->data);
+    }
 }
--- adsaexp/bst_search.c
+++ adsaexp/bst_search.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node {
+struct node              //creation of struc node
+{
     int data;
     struct node* left;
     struct node* right;
 }*root = NULL;
-
+//functions definations
 struct node* insert(struct node* root, int value);
 struct node* createNode(int value);
 void preorder(struct node* root);
@@ -15,99 +16,117 @@ void postorder(struct node* root);
 struct node* Search(struct node* root, int key);
 
 
-void main(){
-
+void main()
+{
     int n;
     printf("Enter the number of elements to be inserted :");
     scanf("%d",&n);
     int a[n];
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
+    {
         scanf("%d",&a[i]);
     }
-    for(int i=0;i<n;i++){
-        root=insert(root,a[i]);
+    for(int i=0;i<n;i++)
+    {
+        root=insert(root,a[i]);             //insertion of elements in bst
     }
-    printf("Preorder Traversal:");
+    printf("Preorder Traversal:");          //traversals
     preorder(root);
     printf("\n");
+
     printf("Postorder Traversal:");
     postorder(root);
     printf("\n");
+
     printf("Inorder Traversal:");
     inorder(root);
+    printf("\n");
+
     int x;
-    printf("\nEnter the number to be searched:");
+    printf("\nEnter the number to be searched:");       //input to be searched
     scanf("%d",&x);
 
-    if(Search(root,x)){
+    if(Search(root,x))              //output by search function
+    {
         printf("\nThe Given key node exists.");
     }
-    else{
+    else
+    {
         printf("\nThe Given Key node doesn't exists.");
     }
 
 }
 
 
-struct node* createNode(int value){
+struct node* createNode(int value)      //creates the node if node doesnt exists
+{
     struct node* newNode=(struct node*)malloc(sizeof(struct node));
     newNode->data=value;
     newNode->left=newNode->right=NULL;
     return newNode;
 }
 
-struct node* insert(struct node* root, int value){
-    if(root == NULL){
+struct node* insert(struct node* root, int value)       
+{
+    if(root == NULL)            //inserts based on condition of binary properties
+    {
         return createNode(value);
     }
-    if(value<root->data){
+    if(value<root->data)
+    {
         root->left=insert(root->left,value);
     }
-    else{
+    else
+    {
         root->right=insert(root->right, value);
     }
     return root;
 
 }
 
-void preorder(struct node* root){
+void preorder(struct node* root)        //preorder traversal Root-L-R
+{
     if(root != NULL)
- {
- printf("%d\t", root->data);
- preorder(root->left);
- preorder(root->right);
- }
+    {
+    printf("\t%d", root->data);
+    preorder(root->left);
+    preorder(root->right);
+    }
 }
 
-void inorder(struct node* root){
+void inorder(struct node* root)     //inorder traversal L-Root-R
+{
     if(root != NULL)
- {
-
- inorder(root->left);
-  printf("%d\t", root->data);
- inorder(root->right);
- }
+    {
+    inorder(root->left);
+    printf("\t%d", root->data);
+    inorder(root->right);
+    }
 }
 
-void postorder(struct node* root){
+void postorder(struct node* root)        //postorder traversal L-R-Root
+{
     if(root != NULL)
- {
+    {
     postorder(root->left);
     postorder(root->right);
-    printf("%d\t", root->data);
- }
+    printf("\t%d", root->data);
+    }
 }
 
 
-struct node* Search(struct node* root, int key)
+struct node* Search(struct node* root, int key)     //traverse and compare the value to each node
 {
-    if(root==NULL || root->data==key){
+    if(root==NULL || root->data==key)
+    {
         return root;
     }
-    if(key<root->data){
+    if(key<root->data)
+    {
         return Search(root->left,key);
     }
-    else{
+    else
+    {
         return Search(root->right,key);
     }
 }
