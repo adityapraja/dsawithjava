76ad86b9782878712b5a596bcbedf671c52b67bc
adityapraja <206730797+adityapraja@users.noreply.github.com>
2025-08-12 17:07:08 +0000

    Fixed the indentations and added comments.

--- adsaexp/hashfunc.java
+++ adsaexp/hashfunc.java
@@ -1,32 +1,38 @@
 import java.util.Scanner;
 
-public class hashfunc{
-    public static void main(String[] args){
+public class hashfunc
+{
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the string:");
         String str= scanner.nextLine();
         int n= str.length();
         int s =0;
-        for(int i=0;i<n;i++){
-            s=s+ascii(str.charAt(i));
+        for(int i=0;i<n;i++)
+        {
+            s=s+ascii(str.charAt(i));           //sums the ascii values of string
         }
         hashfunction(s,str);
     }
 
-    public static char ascii(char a){
-        int asciiVal=a;
+    public static char ascii(char a)
+    {
+        int asciiVal=a;         //typecasts the string to int for ascii values
         return a;
     }
 
-    public static void hashfunction(int s,String str){
+    public static void hashfunction(int s,String str)
+    {
         Scanner scanner= new Scanner(System.in);
         System.out.print("Enter the Size of Hash:");
         int n= scanner.nextInt();
-        int j=s%n;
+        int j=s%n;              //calculates the index of hash
         String[] hash = new String[n];
-        hash[j]=str;
-        for(int i=0;i<10;i++){
-            System.out.println(i + ":"+ hash[i]);
+        hash[j]=str;        //assigns the string to that index
+        for(int i=0;i<10;i++)
+        {
+            System.out.println(i + ":"+ hash[i]);       //display of hash
         }
 
     }
