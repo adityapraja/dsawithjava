d12d39dfe4f2d1352bb43a774a784ca1507a0cc0
adityapraja <206730797+adityapraja@users.noreply.github.com>
2025-09-10 03:53:29 +0000

    lcs code

---
+++ adsaexp/lcs.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class lcs 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter the String 1:");
+        String string1= sc.nextLine();
+
+        System.out.println("Enter the String 2:");
+        String string2= sc.nextLine();
+        int n= string1.length();
+        int m= string2.length();
+        // System.out.println(n +" "+ m);
+        int arr[][] =  new int[n+1][m+1];
+        
+        for(int i=0;i<n+1;i++){
+            arr[i][0]=0;
+        }
+
+        for(int j=0;j<m+1;j++){
+             arr[0][j]=0;
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(string1.charAt(i-1)==string2.charAt(j-1)){
+                    arr[i][j] = 1 + arr[i-1][j-1];
+                }
+                else{
+                    arr[i][j]= Math.max(arr[i-1][j],arr[i][j-1]);
+                }
+        }
+
+    }
+  
+    
+
+    System.out.println("LCS: "+ arr[n][m]);
+    int a=n,b=m;
+    while (a > 0 && b > 0) {
+        if (string1.charAt(a - 1) == string2.charAt(b - 1)) {
+                System.out.print(string1.charAt(a - 1));
+                a--;
+                b--;
+            } else if (arr[a - 1][b] > arr[a][b - 1]) {
+                a--;
+            } else {
+                b--;
+            }
+        }
+    sc.close();
+    }
+}
+
